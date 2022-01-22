7eff86d8408ce2f466e3664120df8a2982d63d10
panda467 <92750571+panda467@users.noreply.github.com>
2022-01-22 00:23:07 -0300

    versão1

--- crud/src/database/index.ts
+++ crud/src/database/index.ts
@@ -1,3 +1,4 @@
 import { createConnection } from "typeorm";
 
-createConnection().then(function(){ console.log('Conexao com o banco de dados foi realizado!');}).catch(function (err) { console.log('Houve um problema de conexão com o banco de dados!, cache: ',err)});
+
+createConnection().then(function(){ console.log('Conexao com o banco de dados foi realizado!')}).catch(function (err) { console.log('Houve um problema de conexão com o banco de dados!, cache: ',err)});
--- crud/src/routes.ts
+++ crud/src/routes.ts
@@ -18,12 +18,12 @@ const routes = Router();
 
 
 
-/**                                     Coversa de homens palitos:
- * [X] C - CREATE - POST                  - voce não acha estranho 
- * [X] R - READ   - GET        o     o       que um ser de outra dimensão nos observe?
- * [X] U - UPDATE - PUT       (|\   /|\
- * [X] D - DELETE - DELETE     |)   /|     - sim um pouco, sera que estamos vivendo na matrix? #perguntas sem respostas 
-*/                                            
+/**                        
+ * [X] C - CREATE - POST   
+ * [X] R - READ   - GET    
+ * [X] U - UPDATE - PUT    
+ * [X] D - DELETE - DELETE 
+*/                         
 
 
 
@@ -39,12 +39,4 @@ routes.get("/produtos",new GetAllProductsControllers().handle);
 routes.delete("/produtos/:id",new DeleteProductsControllers().handle);
 routes.put("/produtos/:id",new UpdateProductsControllers().handle);
 
-
-export{ routes };
-
-
-//     hello!, passei só para te dar um oi mas lebre-se: 
-//      se me ouvir falando talves seja hora de ir dormir programador!
-//  \o
-//   |\
-//   |\
+export { routes };
