86310d6cb5cc85852fb693b295ce04f1a8b6ed08
panda467 <92750571+panda467@users.noreply.github.com>
2022-01-23 22:18:45 -0300

    versão1 estavel

--- crud-1/lib/database/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_1 = require("typeorm");
-(0, typeorm_1.createConnection)().then(function () { console.log('Conexao com o banco de dados foi realizado!'); }).catch(function (err) { console.log('Houve um problema de conexão com o banco de dados!, cache: ', err); });
-//# sourceMappingURL=index.js.map
